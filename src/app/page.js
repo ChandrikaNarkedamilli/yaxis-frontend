f7ae3cbed9bcb829ca638f4cc7184456616a061d
ChandrikaNarkedamilli <chandrikanarkedamilli07@gmail.com>
2025-06-01 16:12:50 +0530

    updated homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
+"use client";
 import Link from 'next/link';
 import './globals.css'
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+ const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            try {
+                const payload = JSON.parse(atob(token.split('.')[1]));
+                console.log("Decoded token payload:", payload);
+                setUser(payload);
+            } catch (err) {
+                console.error("Error decoding token:", err);
+            }
+        } else {
+            console.log("No token found in localStorage");
+        }
+    }, []);
   return (
     <>
     <div className="container">
